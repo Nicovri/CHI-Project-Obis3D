71d6f81d5bc962313f561e21b2236dc37f1d1a23
DelamarcheRuben <77103583+DelamarcheRuben@users.noreply.github.com>
2022-06-16 21:11:30 +0200

    Update Individual.java

--- src/model/animal/Individual.java
+++ src/model/animal/Individual.java
@@ -1,5 +1,22 @@
 package model.animal;
 
 public class Individual {
-
+	private String id;
+	private String recordedBy;
+	private Specie specie;
+	
+	public String getId() 
+	{
+		return id;
+	}
+	
+	public String getRecordedBy() 
+	{
+		return recordedBy;
+	}
+	
+	public Specie getSpecie() 
+	{
+		return specie;
+	}
 }
