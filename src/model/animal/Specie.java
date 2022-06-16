2964a7087ae5682e0fafceb1f3da1aa375991a0a
DelamarcheRuben <77103583+DelamarcheRuben@users.noreply.github.com>
2022-06-16 21:11:49 +0200

    Update Specie.java

--- src/model/animal/Specie.java
+++ src/model/animal/Specie.java
@@ -1,5 +1,22 @@
 package model.animal;
 
 public class Specie {
-
+	private String scientificName;
+	private String order;
+	private String superClass;
+	
+	public String getScientificName() 
+	{
+		return scientificName;
+	}
+	
+	public String getOrder() 
+	{
+		return order;
+	}
+	
+	public String getSuperClass() 
+	{
+		return superClass;
+	}
 }
