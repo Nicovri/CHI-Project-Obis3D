4247ef94177be6f194302ac8d51ef43abf546b53
DelamarcheRuben <77103583+DelamarcheRuben@users.noreply.github.com>
2022-06-17 12:45:56 +0200

    Update JSon.java

--- src/model/JSon.java
+++ src/model/JSon.java
@@ -1,13 +1,18 @@
+package model;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javafx.util.Pair;
 import model.geohash.*;
 
 public class JSon {
@@ -22,7 +27,7 @@ public class JSon {
 		}
 		return sb.toString();
 	}
-	
+
 	public static JSONObject getFromFile(String fichier)
 	{
 		try
@@ -39,44 +44,72 @@ public class JSon {
 			return null;
 		}
 	}
-	
+
 	public static JSONObject getFromRequest(String requestUrl)
 	{
 		return null;
 	}
-	
-	public static String buildGeoHashRequest(String s)//String s = GeoHash plus tard
+
+	public static String buildGeoHashRequest(String geohash, String specieName)//String s = GeoHash plus tard
 	{
-		return null;
+		StringBuilder sb = new StringBuilder();
+		sb.append("https://api.obis.org/v3/occurrence?");
+		if(!specieName.equals(""))
+		{
+			specieName = specieName.replace(" ", "&20");
+			sb.append("?scientificname=" + specieName+"&");
+		}
+		sb.append("geometry="+geohash);
+
+		return sb.toString();
 	}
-	
+
 	public static String buildSpecieRequest(String specieName)
 	{
-		return null;
+		StringBuilder sb = new StringBuilder();
+		sb.append("https://api.obis.org/v3/occurrence/");
+		sb.append("?scientificname=" + specieName);
+		return sb.toString();
 	}
-	
+
 	public static String buildSpecieRequest(String specieName, Date startDate, Date endDate)
 	{
 		return null;
 	}
-	
-	public static JSONObject fetchResultSpecieOccurences(JSONObject rawResult)
+
+	public static HashMap<String, Integer> fetchResultSpecieOccurences(JSONObject rawResult)
 	{
-		HashMap<Location, Integer> nbOccurences = new HashMap<Location, Integer>();
-		
-		
+		HashMap<String, Integer> nbOccurences = new HashMap<String, Integer>(); //String = geohash d'une pos
+
+
 		JSONArray resultat = rawResult.getJSONArray("features");
 		resultat.forEach(item -> {
-			JSONObject j = (JSONObject) item;
-			System.out.println(j.getJSONObject("geometry"));
+
+			JSONObject pos = (JSONObject) item;
+			JSONObject coord = pos.getJSONObject("geometry");
+			JSONArray temp = coord.getJSONArray("coordinates");
+			temp.forEach(carre -> {
+				//
+				Pair<BigDecimal, BigDecimal> a, c, milieu; //On veut calculer le centre du carré géo, qui vaut le milieu du segment AC
+				JSONArray temp_coord = (JSONArray) carre;
+				a = new Pair<>(new BigDecimal(temp_coord.getJSONArray(0).get(0).toString()), new BigDecimal(temp_coord.getJSONArray(0).get(1).toString()));
+				c = new Pair<>(new BigDecimal(temp_coord.getJSONArray(2).get(0).toString()), new BigDecimal(temp_coord.getJSONArray(2).get(1).toString()));
+				milieu = new Pair<>((a.getKey().add(c.getKey())).divide(new BigDecimal("2.0")), (a.getValue().add(c.getValue())).divide(new BigDecimal("2.0")));
+				Integer nb = new Integer(pos.getJSONObject("properties").getInt("n"));
+				nbOccurences.put(GeoHashHelper.getGeohash(new Location("", milieu.getKey().doubleValue(), milieu.getValue().doubleValue()), 5), nb);
+			});
 		});
-		JSONObject article = resultat.getJSONObject(0);
-		return article;
+		return nbOccurences;
 	}
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		JSONObject res = fetchResultSpecieOccurences(getFromFile("Delphinidae.json"));
-		//System.out.println(res);
-		
+		HashMap<String, Integer> nbOccurences = fetchResultSpecieOccurences(getFromFile("Delphinidae.json"));
+		/*for(Entry<String, Integer> i : nbOccurences.entrySet())
+		{
+			System.out.println(i);
+		}*/
+		System.out.println(buildGeoHashRequest("spd", "Manta birostris"));
+
 	}
 }
+
