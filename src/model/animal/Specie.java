c52294acf1884d44a1c9e05f8d3078d6a5e876bf
DelamarcheRuben <77103583+DelamarcheRuben@users.noreply.github.com>
2022-06-22 11:38:40 +0200

    Add equals on Specie

--- src/model/animal/Specie.java
+++ src/model/animal/Specie.java
@@ -4,19 +4,39 @@ public class Specie {
 	private String scientificName;
 	private String order;
 	private String superClass;
-	
-	public String getScientificName() 
+
+	public Specie(String scientificName, String order, String superClass)
+	{
+		this.scientificName = scientificName;
+		this.order = order;
+		this.superClass = superClass;
+	}
+
+	public String getScientificName()
 	{
 		return scientificName;
 	}
-	
-	public String getOrder() 
+
+	public String getOrder()
 	{
 		return order;
 	}
-	
-	public String getSuperClass() 
+
+	public String getSuperClass()
 	{
 		return superClass;
 	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if(this==o) return true;
+		if(o == null) return false;
+
+		if(o.getClass()!=this.getClass()) return false;
+		Specie s = (Specie) o;
+		if(this.getScientificName().equals(s.getScientificName())) return true;
+		return false;
+	}
 }
+
