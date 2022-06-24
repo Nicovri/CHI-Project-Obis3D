27a6bfec835f1e17fd37bfd09d81a9e9e387acd7
DelamarcheRuben <77103583+DelamarcheRuben@users.noreply.github.com>
2022-06-24 16:25:20 +0200

    Update RequestIntervalsThread.java

--- src/utils/RequestIntervalsThread.java
+++ src/utils/RequestIntervalsThread.java
@@ -3,8 +3,16 @@ package utils;
 import java.util.List;
 import java.util.Map;
 
-//Classe specifiquement faite pour le fetching des intervalles
-//Peut-etre a mettre directement dans le fetching et non dans une classe a part
+/**
+* Classe specifiquement faite pour le fetching des intervalles
+*
+* @version 1.0.0
+*
+* @auhor Nicolas Vrignaud
+* @auhor Ruben Delamarche
+*
+*/
+
 public class RequestIntervalsThread implements Runnable
 {
 		private int indice; //Indice d'acces a la liste des intervalles
@@ -13,6 +21,9 @@ public class RequestIntervalsThread implements Runnable
 		private String end;
 		private String specieName;
 
+		/**
+		* Constructeur. Il faut la liste des intervals, l'indice d'ajout d'une map dans cette liste, date de début et de fin et un nom d'espece
+		*/
 		public RequestIntervalsThread(List<Map<String, Long>> _intervals, int _indice, String _start, String _end, String _specieName)
 		{
 			indice = _indice;
@@ -25,9 +36,9 @@ public class RequestIntervalsThread implements Runnable
 		@Override
 		public void run()
 		{
-			System.out.println(indice);
+			//On ajoute au bonne indice la map du fetching entre les deux dates que contient le thread actuel
 			intervals.set(indice, Requests.fetchResultSpecieOccurences(Requests.getFromRequest(
 					Requests.buildSpecieRequestWithGrid(specieName, start, end, 3))));
 		}
 
-	}
+}
